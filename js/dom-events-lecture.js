9f67b68cb97a50c1fe8c470442ddb89249c80034
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-02 14:33:26 -0600

    add dom-events-lecture.html, js/dom-events-lecture.js ,and css/dom-events-lecture.css

---
+++ js/dom-events-lecture.js
@@ -0,0 +1,30 @@
+// get access to the button in the DOM
+// var button = document.querySelector("button");
+// Write the event handler
+// aka event listener, callback
+function doThisWhenTheButtonIsClicked() {
+    alert("You clicked me!");
+}
+// Finally, we register the handler
+// button.addEventListener("click", doThisWhenTheButtonIsClicked);
+//
+// var secondButton = document.getElementsByTagName("button")[1];
+//
+// secondButton.onclick = doThisWhenTheButtonIsClicked;
+
+var allTheButtons = document.getElementsByTagName("button");
+for (var i = 0; i < allTheButtons.length; i++) {
+    allTheButtons[i].addEventListener("click", doThisWhenTheButtonIsClicked);
+}
+
+// Use HTML and css to create a square with a background color on a html page.
+// register an event handler and write its code such that when you click on a square it alerts that it is a square and what color it is.
+
+var theSquare = document.querySelector(".square");
+
+function alertStatus () {
+    alert("I am a big red square");
+}
+
+theSquare.addEventListener("click", alertStatus);
+
