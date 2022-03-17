423f0870f6f15dbb4d658e5729f96278a4f2e9aa
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-17 15:25:07 -0500

    js/es6.js

--- js/js-es6-lecture.js
+++ js/js-es6-lecture.js
@@ -1,10 +1,75 @@
-// If I use var inside a code block, the scope of the variable is global
+// If I use var inside a code block
+// , the scope of the variable is global
+// {
+//     var name = 'Javier';
+// }
+// console.log(name);
+
+// If I use let inside a code block, the scope
+// of the variable is local
+// {
+//     let name = 'Dane';
+// }
+// console.log(danesName);
+
+// true will always run
+// while(true){
+//     let firstName = 'Javier';
+//     break;
+// }
+
+// let name = "jojo";
+// if(true) {let name = "javier"};
+// console.log(name);
+
+// let javiersName = 'Javier';
+// let job = 'programmer';
+// let place = 'Texas';
+//
+// let theBigStringInHtml = '<p>${javiersName}' +
+//     ' is a <span>${job}</span> in ${place}.</p>';
+
+// const car = {
+//     make: "Toyota",
+//     model: "Tacoma",
+//     year: 2019,
+//     mileage: 12657
+// }
+
+// let make = car.make;
+// let model = car.model;
+// let year = car.year;
+// let mileage = car.mileage;
+
+// const {make, model, year, mileage} = car;
+//
+// let carDescription = '<p>I see a ${year} ${make} ${model} with ${mileage}</p>';
+//
+// outputDiv.innerHTML += carDescription;
+
+// Example
+// function declaration
+// function ass(num1, num2){
+//     return num1 + num2;
+// }
+
+// function expression
+// const add = function (num1, num2){
+//     return num1 + num2;
+// }
+
+// const add = (num1, num2) => num1 + num2;
+
+// const add10 = num => num+10;
+//
+// const helloWorld = () => "Hello World";
 
 
-{
-    let name = 'Dane';
-}
-console.log(danesName);
-
+// const instructors = ["Javier", "Justin", "Jay", "David"];
+//
+// for (const name of instructors){
+//     console.log(('${name'} teaches at Codeup');' +
+// ')
+// }
 
 
