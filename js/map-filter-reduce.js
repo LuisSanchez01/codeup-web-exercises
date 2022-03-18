e7b798d761e773e9754a9a8be220cc917ce2269b
Luis Sanchez <sanchez.alberto.luis01@gmail.com>
2022-03-18 16:57:13 -0500

    add map-filter-reduce.js exercises

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,88 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+// Example
+// const bestShow = arrayOfTVShows.filter((show) => show.imdbScore >= 9 && show.firstEpisodeYear >= 2008)
+//
+// console.log("bestShow:", bestShow);
+
+const threeLanguages = users.filter((languages) => languages.languages.length >= 3)
+console.log("threeLanguages:", threeLanguages);
+
+// Use .map to create an array of strings where each element is a user's email address
+
+const usersEmail = users.map(({email}) => email);
+console.log("usersEmail:", usersEmail)
+
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can
+// use the result to calculate the average.
+
+const usersYearsOfExperienceSum = users.reduce((total, user) => {
+    return total + user.yearsOfExperience;
+}, 0);
+
+console.log("usersYearsOfExperienceSum:", usersYearsOfExperienceSum);
+
+
+// Use .reduce to get the longest email from the list of users.
+
+const longestEmail = users.reduce((email, user) => {
+    if (user.email.length <= email.length) {
+        if (user.email.length < email.length) {
+            return email;
+        }
+    } else {
+        return user.email;
+    }
+},'')
+
+console.log("longestEmail:", longestEmail)
+
+// ternary operator to get result
+
+
+// Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, ' +
+// 'luis, zach, fernando, justin.
+
+// const longestEmail = users.reduce((email, user) => {
+//     if (user.email.length > email.length) email.push(user)
+//     return email + user.email;
+// }, 0);
+//
+// console.log("longestEmail:", longestEmail)
